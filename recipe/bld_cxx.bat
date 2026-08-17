47ffe3f22c16d830a3f523cb350087c16fd30335
Silvio Traversaro <silvio@traversaro.it>
2026-08-17 22:09:12 +0200

    Explicity enable YARP devices

--- recipe/bld_cxx.bat
+++ recipe/bld_cxx.bat
@@ -19,6 +19,9 @@ cmake -G "Ninja" ^
     -DFRAMEWORK_USE_PCL:BOOL=ON ^
     -DFRAMEWORK_COMPILE_PYTHON_BINDINGS:BOOL=OFF ^
     -DFRAMEWORK_COMPILE_Ros1Publisher:BOOL=OFF ^
+    -DFRAMEWORK_COMPILE_FloatingBaseEstimators:BOOL=ON ^
+    -DFRAMEWORK_COMPILE_RobotDynamicsEstimator:BOOL=ON ^
+    -DFRAMEWORK_COMPILE_JointTorqueControlDevice:BOOL=ON ^
     -DUSE_SYSTEM_Catch2:BOOL=ON ^
     -DUSE_SYSTEM_tiny-process-library:BOOL=ON ^
     %SRC_DIR%
